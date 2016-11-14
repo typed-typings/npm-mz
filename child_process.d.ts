5fdd82eac73b11b9fcedc2a57b5c9a7deb438491
Felix Becker <felix.b@outlook.com>
2016-11-14 23:54:49 +0100

    Update node typings

--- child_process.d.ts
+++ child_process.d.ts
@@ -1,26 +1,15 @@
 
-import {
-    ExecFileOptions,
-    ExecFileOptionsWithBufferEncoding,
-    ExecFileOptionsWithStringEncoding,
-    ExecOptions,
-    ExecOptionsWithBufferEncoding,
-    ExecOptionsWithStringEncoding,
-    ChildProcess
-} from 'child_process';
+import {ExecFileOptions, ExecOptions} from 'child_process';
 
 export * from 'child_process';
 
 export function exec(command: string): Promise<[string, string]>;
-export function exec(command: string, options: ExecOptionsWithStringEncoding): Promise<[string, string]>;
-export function exec(command: string, options: ExecOptionsWithBufferEncoding): Promise<[Buffer, Buffer]>;
+export function exec(command: string, options: ExecOptions & { encoding: 'buffer' }): Promise<[Buffer, Buffer]>;
 export function exec(command: string, options: ExecOptions): Promise<[string, string]>;
 
 export function execFile(file: string): Promise<[string, string]>;
-export function execFile(file: string, options?: ExecFileOptionsWithStringEncoding): Promise<[string, string]>;
-export function execFile(file: string, options?: ExecFileOptionsWithBufferEncoding): Promise<[Buffer, Buffer]>;
+export function execFile(file: string, options?: ExecFileOptions & { encoding: 'buffer' }): Promise<[Buffer, Buffer]>;
 export function execFile(file: string, options?: ExecFileOptions): Promise<[string, string]>;
 export function execFile(file: string, args?: string[]): Promise<[string, string]>;
-export function execFile(file: string, args?: string[], options?: ExecFileOptionsWithStringEncoding): Promise<[string, string]>;
-export function execFile(file: string, args?: string[], options?: ExecFileOptionsWithBufferEncoding): Promise<[Buffer, Buffer]>;
+export function execFile(file: string, args?: string[], options?: ExecFileOptions & { encoding: 'buffer' }): Promise<[Buffer, Buffer]>;
 export function execFile(file: string, args?: string[], options?: ExecFileOptions): Promise<[string, string]>;
--- readline.d.ts
+++ readline.d.ts
@@ -1,10 +1,10 @@
 
-import {ReadLine as NodeReadLine, Completer, ReadLineOptions} from 'readline';
+import {Interface, InterfaceOptions, Completer} from 'readline';
 export * from 'readline';
 
-export interface ReadLine extends NodeReadLine {
+export interface ReadLine extends Interface {
     question(query: string): Promise<string>;
 }
 
 export function createInterface(input: NodeJS.ReadableStream, output?: NodeJS.WritableStream, completer?: Completer, terminal?: boolean): ReadLine;
-export function createInterface(options: ReadLineOptions): ReadLine;
+export function createInterface(options: InterfaceOptions): ReadLine;
